40d6bddfec9eac57a986ed92d797a18ea4ec6191
Matteo <matteo.civitillo@mail.polimi.it>
2024-03-18 15:32:43 +0100

    Changes by acall

---
+++ src/main/java/org/example/model/deck/CardRes.java
@@ -0,0 +1,5 @@
+package org.example.model.deck;
+
+public enum CardRes {
+    PLANT, ANIMAL, FUNGI, INSECT
+}
---
+++ src/main/java/org/example/model/deck/PropertiesCorner.java
@@ -0,0 +1,5 @@
+package org.example.model.deck;
+
+public enum PropertiesCorner {
+    EMPTY, HIDDEN, PLANT, ANIMAL, FUNGI, INSECT, QUILL, INKWELL, MANUSCRIPT
+}
