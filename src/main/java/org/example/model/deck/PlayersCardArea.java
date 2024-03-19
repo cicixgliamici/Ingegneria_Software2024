81f613176e50a23324792590d80fbe02364cc78a
acall <alessandro.callegari@mail.polimi.it>
2024-03-19 15:27:30 +0100

    .

---
+++ src/main/java/org/example/model/deck/PlayersCardArea.java
@@ -0,0 +1,11 @@
+package org.example.model.deck;
+
+import java.util.List;
+
+public class PlayersCardArea {
+    private Card[][] PlayedCard;
+    private List<CardRes>  PlayerRes;
+    private List<PropertiesCorner> PlayerObject;
+
+    public Card[][] InitialisizedPlayerArea
+}
