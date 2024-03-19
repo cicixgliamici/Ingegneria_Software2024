0d5db27ab9ce02d70473e36c38c042a99064d2cc
Leonardo <10720295@polimi.it>
2024-03-19 15:45:45 +0100

    I Test non funzionano

--- src/test/java/org/example/DeckTest.java
+++ src/test/java/org/example/DeckTest.java
@@ -1,32 +1,34 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.example.model.deck.*;
 
 public class DeckTest {
 
-    @Test
+
     public void testDeckResource() {
-        Deck deck = new Deck(Type.RESOURCE);
-        assertEquals(Type.RESOURCE, deck.getType());
+        Deck deck = new Deck(Type.RESOURCES);
+        assertEquals(Type.RESOURCES, deck.getType());
         assertEquals(40, deck.getCardNumbers());
     }
 
-    @Test
+
     public void testDeckResource() {
         Deck deck = new Deck(Type.GOLD);
         assertEquals(Type.GOLD, deck.getType());
         assertEquals(40, deck.getCardNumbers());
     }
 
-    @Test
+
     public void testDeckResource() {
         Deck deck = new Deck(Type.OBJECT);
         assertEquals(Type.OBJECT, deck.getType());
         assertEquals(16, deck.getCardNumbers());
     }
 
-    @Test
+
     public void testDeckResource() {
         Deck deck = new Deck(Type.STARTER);
         assertEquals(Type.STARTER, deck.getType());
