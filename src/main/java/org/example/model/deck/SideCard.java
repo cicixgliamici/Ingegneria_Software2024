e8b92b5594022829aa71ca85809fbefd3d9e0f03
jamie <jamesenrico.busato@mail.polimi.it>
2024-03-19 15:15:15 +0100

    uh

--- src/main/java/org/example/model/deck/SideCard.java
+++ src/main/java/org/example/model/deck/SideCard.java
@@ -3,6 +3,17 @@ package org.example.model.deck;
 public class SideCard {
     private Side side;
     private Corner[] corners;
+    public SideCard(Side side, PropertiesCorner prop1,
+                               PropertiesCorner prop2,
+                               PropertiesCorner prop3,
+                               PropertiesCorner prop4) {
+        this.side= side;
+        corners = new Corner[4];
+        corners[0] = new Corner(Position.TOPL, prop1);
+        corners[1] = new Corner(Position.TOPR, prop2);
+        corners[2] = new Corner(Position.BOTTOMR, prop3);
+        corners[3] = new Corner(Position.BOTTOML, prop4);
+    }
 
     public Side getSide() {
         return side;
