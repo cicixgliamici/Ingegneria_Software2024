60237b802ab247501a39bb0df6f4055d264b0ba6
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-03-19 15:00:07 +0100

    Update Deck.java

--- src/main/java/org/example/model/deck/Deck.java
+++ src/main/java/org/example/model/deck/Deck.java
@@ -1,7 +1,32 @@
 package org.example.model.deck;
 
-public interface Deck {
-    void shuffle();
-    void initializeDeck(Type type);
+public class Deck {
+    
+    private Type type;
+    private int CardNumbers;
+
+     public Deck(Type type) {
+        this.type = type;
+        switch (type) {
+            case RESOURCES:
+                this.cardNumbers = 40;
+                // CHIAMARE FUNZIONE GENERA CARTE
+                break;
+            case GOLD:
+                this.cardNumbers = 40; 
+                 // CHIAMARE FUNZIONE GENERA CARTE
+                break;
+            case OBJECT:
+                this.cardNumbers = 16; 
+                 // CHIAMARE FUNZIONE GENERA CARTE
+                break;
+            case STARTER:
+                this.cardNumbers = 6; 
+                 // CHIAMARE FUNZIONE GENERA CARTE
+                break;
+            default:
+                this.cardNumbers = 0; 
+        }
+    }
 }
 
