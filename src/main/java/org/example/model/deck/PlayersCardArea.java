195c87037adea9cffc52495c6898ff1ca7857758
acall <alessandro.callegari@mail.polimi.it>
2024-03-25 12:16:40 +0100

    Odissea nella struttura dati del tavolo da gioco. Manca l'aggionramento delle risorse al piazzamento delle carte

--- src/main/java/org/example/model/deck/PlayersCardArea.java
+++ src/main/java/org/example/model/deck/PlayersCardArea.java
@@ -34,8 +34,62 @@ public class PlayersCardArea {
         }
     }
 
+    public int SearchCardMatrixToLinkCornerColumn(Card searchCard, Corner linkCorner){
+        int j = 0;
+        int i = 0;
+        while ((this.PlayedCard[i][j].getCardCell() != searchCard) && (this.PlayedCard[i][j].getAvailabilityCorner() != linkCorner.getPosition())){
+            while ((this.PlayedCard[i][j].getCardCell() != searchCard) && (this.PlayedCard[i][j].getAvailabilityCorner() != linkCorner.getPosition())) {
+                i++;
+            };
+            j++;
+        };
+        return i;
+    }
+
+    public int SearchCardMatrixToLinkCornerLine(Card searchCard, Corner linkCorner){
+        int j = 0;
+        int i = 0;
+        while ((this.PlayedCard[i][j].getCardCell() != searchCard) && (this.PlayedCard[i][j].getAvailabilityCorner() != linkCorner.getPosition())){
+            while ((this.PlayedCard[i][j].getCardCell() != searchCard) && (this.PlayedCard[i][j].getAvailabilityCorner() != linkCorner.getPosition())) {
+                i++;
+            };
+            j++;
+        };
+        return j;
+    } //tocca trovare un search che sia più elegante e che non faccia venire un infarto a bill gates
     public void InsertNewCard(Card TouchedCard, Card NewCard, Corner ChoosenCorner){
-        //cerco la carta, individuo l'angolo dove devo inserire la carta nuova giocando con gli indici della matrice
+        if (ChoosenCorner.getPosition() == Position.TOPL){
+            //aggiungere TOPR alla posizione i+1 j
+            ModifyPlayedCard((SearchCardMatrixToLinkCornerColumn(TouchedCard, ChoosenCorner)) + 1, (SearchCardMatrixToLinkCornerColumn(TouchedCard,ChoosenCorner)), Position.TOPR, NewCard);
+            //aggiungere BOTTOML alla posizione i j-1
+            ModifyPlayedCard((SearchCardMatrixToLinkCornerColumn(TouchedCard, ChoosenCorner)) , (SearchCardMatrixToLinkCornerColumn(TouchedCard,ChoosenCorner)) - 1, Position.BOTTOML, NewCard);
+            //aggiungere TOPL alla posizione i-1 j-1
+            ModifyPlayedCard((SearchCardMatrixToLinkCornerColumn(TouchedCard, ChoosenCorner)) - 1, (SearchCardMatrixToLinkCornerColumn(TouchedCard,ChoosenCorner)) - 1, Position.TOPL, NewCard);
+        }
+        else if (ChoosenCorner.getPosition() == Position.TOPR){
+            //aggiungere TOPR alla posizione  i+1 j+1
+            ModifyPlayedCard((SearchCardMatrixToLinkCornerColumn(TouchedCard, ChoosenCorner)) + 1, (SearchCardMatrixToLinkCornerColumn(TouchedCard,ChoosenCorner)) + 1, Position.TOPR, NewCard);
+            //aggiungere BOTTOMR alla posizione i j+1
+            ModifyPlayedCard((SearchCardMatrixToLinkCornerColumn(TouchedCard, ChoosenCorner)), (SearchCardMatrixToLinkCornerColumn(TouchedCard,ChoosenCorner)) + 1, Position.BOTTOMR, NewCard);
+            //aggiungere TOPL alla poszione TOPL i-1 j
+            ModifyPlayedCard((SearchCardMatrixToLinkCornerColumn(TouchedCard, ChoosenCorner)) - 1, (SearchCardMatrixToLinkCornerColumn(TouchedCard,ChoosenCorner)), Position.TOPL, NewCard);
+        }
+        else if (ChoosenCorner.getPosition() == Position.BOTTOMR){
+            //aggiungere TOPR alla posizione i j+1
+            ModifyPlayedCard((SearchCardMatrixToLinkCornerColumn(TouchedCard, ChoosenCorner)), (SearchCardMatrixToLinkCornerColumn(TouchedCard,ChoosenCorner)) + 1, Position.TOPR, NewCard);
+            //aggiungere BOTTOMR i+1 j+1
+            ModifyPlayedCard((SearchCardMatrixToLinkCornerColumn(TouchedCard, ChoosenCorner)) + 1, (SearchCardMatrixToLinkCornerColumn(TouchedCard,ChoosenCorner)) + 1, Position.BOTTOMR, NewCard);
+            //aggiungere BOTTOML i+1 j
+            ModifyPlayedCard((SearchCardMatrixToLinkCornerColumn(TouchedCard, ChoosenCorner)) + 1, (SearchCardMatrixToLinkCornerColumn(TouchedCard,ChoosenCorner)), Position.BOTTOML, NewCard);
+        }
+        else if(ChoosenCorner.getPosition() == Position.BOTTOML){
+            //aggiungere TOPL i j-1
+            ModifyPlayedCard((SearchCardMatrixToLinkCornerColumn(TouchedCard, ChoosenCorner)), (SearchCardMatrixToLinkCornerColumn(TouchedCard,ChoosenCorner)) - 1, Position.TOPL, NewCard);
+            //aggiungere BOTTOMR i +1 j
+            ModifyPlayedCard((SearchCardMatrixToLinkCornerColumn(TouchedCard, ChoosenCorner)) + 1, (SearchCardMatrixToLinkCornerColumn(TouchedCard,ChoosenCorner)), Position.BOTTOMR, NewCard);
+            //aggiungere BOTTOML i+1 j-1
+            ModifyPlayedCard((SearchCardMatrixToLinkCornerColumn(TouchedCard, ChoosenCorner)) + 1, (SearchCardMatrixToLinkCornerColumn(TouchedCard,ChoosenCorner)) - 1, Position.BOTTOML, NewCard);
+        }
     }
 
 }
