6d8257155b5553db9a2fa4672d60c4ae4bedbbf3
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-04-23 13:10:41 +0200

    Tony Tony Tony

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -3,8 +3,20 @@ package org.example;
 import org.example.model.deck.Card;
 
 public class Server {
+    private boolean chooseMode;
+    private int numPlayers;
+    
     public Card getCard(){
         //todo metodo che chiede al client una carta dalla mano
         return new Card();
+    }
+    public int getNumPlayers() {
+        return numPlayers;
+    }
+    public void setChooseMode(boolean chooseMode) {
+        this.chooseMode = chooseMode;
+    }
+    public boolean isChooseMode() {
+        return chooseMode;
     }
 }
