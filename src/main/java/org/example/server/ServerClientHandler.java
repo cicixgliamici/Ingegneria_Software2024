5e5fac85f05f236abd89c71d7ab698f21701a434
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-05-06 16:50:03 +0200

    Update ServerClientHandler.java

--- src/main/java/org/example/server/ServerClientHandler.java
+++ src/main/java/org/example/server/ServerClientHandler.java
@@ -17,6 +17,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/** Class for the interpretation of the commands written by the client
+*/
+
 public class ServerClientHandler implements Runnable {
     private Socket socket;
     private Model model;
@@ -42,8 +45,7 @@ public class ServerClientHandler implements Runnable {
                     out.println("Command not recognized.");
                 }
             }
-
-            // Chiudo gli stream e il socket
+            // Close stream and socket
             in.close();
             out.close();
             socket.close();
@@ -52,37 +54,35 @@ public class ServerClientHandler implements Runnable {
         }
     }
 
-    //i comandi devono essere tutti del tipo draw:1 oppure play:1,2,3,4
+    /** Read a string written from the client which must have this format:  method:param1,param2,param3
+    *   If there is a method with the same name in the Commands.JSON, calls it with the parameters.
+    *   EX: draw:int
+    *       play:int,int,int,int
+    */
     private void executeCommand(String inputLine, PrintWriter out) {
         try {
             String[] parts = inputLine.split(":");
             String commandKey = parts[0];
             String[] params = parts.length > 1 ? parts[1].split(",") : new String[0];
-
             if (!commands.containsKey(commandKey)) {
                 out.println("Command not recognized.");
                 return;
             }
-
             JSONObject commandDetails = commands.get(commandKey);
             String className = commandDetails.getString("className");
             String methodName = commandDetails.getString("methodName");
             JSONArray jsonParams = commandDetails.getJSONArray("parameters");
-
             Class<?> cls = Class.forName(className);
             Class<?>[] paramTypes = new Class[jsonParams.length()];
             Object[] paramValues = new Object[jsonParams.length()];
-
             for (int i = 0; i < jsonParams.length(); i++) {
                 JSONObject param = jsonParams.getJSONObject(i);
                 String type = param.getString("type");
                 paramTypes[i] = type.equals("int") ? int.class : Class.forName(type);
                 paramValues[i] = type.equals("Model") ? model : Integer.parseInt(params[i]); // Assicurati che l'indice esista
             }
-
             Method method = cls.getDeclaredMethod(methodName, paramTypes);
             Object response = method.invoke(controller, paramValues);
-
             out.println("Command executed: " + response.toString());
         } catch (Exception e) {
             out.println("Error executing command: " + e.getMessage());
