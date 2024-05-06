caf786c63f635eb27252b399b99b055547163a3c
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-05-06 16:59:51 +0200

    Update CodexNaturalis.java

--- src/main/java/org/example/CodexNaturalis.java
+++ src/main/java/org/example/CodexNaturalis.java
@@ -6,18 +6,20 @@ import org.example.server.PortSelection;
 
 import java.util.Scanner;
 
+/** First class runned when you start the CodexNaturalis.jar
+*   So that you can choose to run a server or tu run a client (TUI or GUI)
+*/
+
 public class CodexNaturalis {
     public static void main(String[] args) {
         System.out.println("WELCOME! What do you want to launch?");
-        System.out.println("1. SERVER\n2. CLIENT (CLI INTERFACE)\n3. CLIENT (GUI INTERFACE)");
+        System.out.println("1. SERVER\n2. CLIENT (TUI INTERFACE)\n3. CLIENT (GUI INTERFACE)");
         Scanner scanner = new Scanner(System.in);
         String input;
         do {
             System.out.println("Insert your choice: ");
             input = scanner.nextLine();
         }while (!(input.equals("1") || input.equals("2") || input.equals("3")));
-
-
         switch (input){
             case "1":
                 PortSelection.main(null);
@@ -29,10 +31,8 @@ public class CodexNaturalis {
                 System.out.println("non valido ancora");
                 //ClientPortSelection.main(null);
                 break;
-
             default:
                 System.out.println("invalid input!\n");
-
         }
     }
 
