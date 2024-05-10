a77f0268ef4d908e39aba2e03bffe75fd164d0b6
acall <alessandro.callegari@mail.polimi.it>
2024-05-10 13:00:38 +0200

    se tu dici io parto, addio

---
+++ src/main/java/org/example/view/GUI/MyDrawImage.java
@@ -0,0 +1,34 @@
+package org.example.view.GUI;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyDrawImage {
+    private Graphics graphics;
+    private int x;
+    private int y;
+    private String fileName;
+
+    public MyDrawImage(Graphics graphics, int x, int y, String fileName){
+        this.graphics = graphics;
+        this.x = x;
+        this.y = y;
+        this.fileName = fileName;
+    }
+
+    public void Draw(){
+        ClassLoader cl = this.getClass().getClassLoader();
+        InputStream url = cl.getResourceAsStream(fileName);
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(url);
+        } catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+        graphics.drawImage(img, x,y,null);
+    }
+}
