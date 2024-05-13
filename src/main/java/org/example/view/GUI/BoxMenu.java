dc5cad520f5e399e88fb7cb6dd96e07450ec2bbb
acall <alessandro.callegari@mail.polimi.it>
2024-05-13 16:45:30 +0200

    swing swing

--- src/main/java/org/example/view/GUI/BoxMenu.java
+++ src/main/java/org/example/view/GUI/BoxMenu.java
@@ -25,7 +25,7 @@ public class BoxMenu extends JPanel{
         setLayout(new GridBagLayout());
 
         // Components
-        BufferedImage logo = ImageIO.read(new File("src/main/resources/logomini.jpg"));
+        BufferedImage logo = ImageIO.read(new File("src/main/resources/images/logomini.jpg"));
         Icon icon = new ImageIcon(logo);
         labelTitle = new JLabel(icon);
 
