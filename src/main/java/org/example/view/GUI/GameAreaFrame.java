d97fd0f39cebb8b0a76d27bedaae9144c0f9b5f8
jamie <jamesenrico.busato@mail.polimi.it>
2024-05-14 17:16:36 +0200

    gui

--- src/main/java/org/example/view/GUI/GameAreaFrame.java
+++ src/main/java/org/example/view/GUI/GameAreaFrame.java
@@ -16,27 +16,52 @@ import java.util.List;
 
 public class GameAreaFrame extends JFrame {
     GameAreaPanel gameAreaPanel;
-    private JPanel background;
+
+    private JButton button1;
+    private JButton button2;
 
     public GameAreaFrame() throws IOException {
         super("Codex Naturalis");
-        setSize(1300, 800);
+        setSize(1300, 840);
         Image icon = Toolkit.getDefaultToolkit().getImage("src/main/resources/images/iconamini.png");
         setIconImage(icon);
+        setLayout(new GridBagLayout());
+        button1 = new JButton("negro");
+        button2 = new JButton("frocio");
 
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx=0;
+        gbc.gridy = 0;
+        gbc.weighty = 0.0;
+        gbc.weightx=0.2;
+
+        add(button1, gbc);
 
         gameAreaPanel = new GameAreaPanel(){
-            //ImageIcon icon = new ImageIcon(ImageIO.read(new File("src/main/resources/images/gamearea.jpg")));
-            //Image img = icon.getImage();
-            //{setOpaque(false);}
-            //public void paintComponent(Graphics graphics){
-              //  graphics.drawImage(img,0,0, this);
-              //  super.paintComponent(graphics);
-            //}
+            ImageIcon icon = new ImageIcon(ImageIO.read(new File("src/main/resources/images/gamearea.png")));
+            Image img = icon.getImage();
+            {setOpaque(false);}
+            public void paintComponent(Graphics graphics){
+                graphics.drawImage(img,0,0, this);
+                super.paintComponent(graphics);
+            }
         };
+        gbc.gridx=1;
+        gbc.gridy = 0;
+        gbc.weighty = 1;
+        gbc.weightx=0.8;
+        gbc.fill = GridBagConstraints.BOTH;
 
-        setLayout(new BorderLayout());
-        add(gameAreaPanel, BorderLayout.CENTER);
+        add(gameAreaPanel, gbc);
+
+        gbc.gridx=2;
+        gbc.gridy = 0;
+        gbc.weighty = 0.0;
+        gbc.weightx=0.2;
+        gbc.fill = GridBagConstraints.NONE;
+        add(button2, gbc);
+
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
--- src/main/java/org/example/view/GUI/GameAreaPanel.java
+++ src/main/java/org/example/view/GUI/GameAreaPanel.java
@@ -24,25 +24,26 @@ public class GameAreaPanel extends JPanel{
     public GameAreaPanel() throws IOException {
         setLayout(new GridBagLayout());
 
-        BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/logomini.jpg"));
+
+        BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_bleu.png"));
         Icon icon1 = new ImageIcon(logo1);
         token1 = new JLabel(icon1);
 
-        BufferedImage logo2 = ImageIO.read(new File("src/main/resources/images/logomini.jpg"));
+        BufferedImage logo2 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_jaune.png"));
         Icon icon2 = new ImageIcon(logo2);
         token2 = new JLabel(icon2);
 
-        BufferedImage logo3 = ImageIO.read(new File("src/main/resources/images/logomini.jpg"));
+        BufferedImage logo3 = ImageIO.read(new File("src/main/resources/images/red.png"));
         Icon icon3 = new ImageIcon(logo3);
         token3 = new JLabel(icon3);
 
-        BufferedImage logo4 = ImageIO.read(new File("src/main/resources/images/logomini.jpg"));
+        BufferedImage logo4 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_vert.png"));
         Icon icon4 = new ImageIcon(logo4);
         token4 = new JLabel(icon4);
 
         GridBagConstraints gbc = new GridBagConstraints();
 
-        gbc.gridx = 1;
+        gbc.gridx = 0;
         gbc.gridy = 2;
 
         gbc.weightx = 0.01;
@@ -50,7 +51,7 @@ public class GameAreaPanel extends JPanel{
 
         add(token1, gbc);
 
-        gbc.gridx = 3;
+        gbc.gridx = 2;
         gbc.gridy = 2;
 
         gbc.weightx = 0.01;
@@ -58,7 +59,7 @@ public class GameAreaPanel extends JPanel{
 
         add(token2, gbc);
 
-        gbc.gridx = 1;
+        gbc.gridx = 0;
         gbc.gridy = 0;
 
         gbc.weightx = 0.01;
@@ -66,7 +67,7 @@ public class GameAreaPanel extends JPanel{
 
         add(token3, gbc);
 
-        gbc.gridx = 3;
+        gbc.gridx = 2;
         gbc.gridy = 0;
 
         gbc.weightx = 0.01;
