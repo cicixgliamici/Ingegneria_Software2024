e6d5d1e9827fbca81e4d6dd8a189976878b94a1a
matteoCivitillo <matteo.civitillo@mail.polimi.it>
2024-05-30 12:16:40 +0200

    Supremazia incontrastata Server Team

--- src/main/java/org/example/view/gui/gamearea4/GameAreaPanel.java
+++ src/main/java/org/example/view/gui/gamearea4/GameAreaPanel.java
@@ -5,6 +5,7 @@ import org.example.view.gui.utilities.Coordinates;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
@@ -17,8 +18,10 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-public class GameAreaPanel extends JPanel{
+public class GameAreaPanel extends JPanel {
     private JLabel token1;
     private JLabel token2;
     private JLabel token3;
@@ -30,7 +33,12 @@ public class GameAreaPanel extends JPanel{
     private JLabel card3;
     private JLabel secretObjective;
     private BufferedImage backgroundImg;
+    private int ChosenId;
+    private int ChosenSide;
     View view;
+    private Map<JLabel, Boolean> cardStates = new HashMap<>();
+    private Map<JLabel, Integer> cardIds = new HashMap<>();
+    private JLabel selectedCard = null;
 
     public GameAreaPanel(View view, String color, String num, String starterCard, String objCard) throws IOException {
         this.view = view;
@@ -38,180 +46,59 @@ public class GameAreaPanel extends JPanel{
 
         System.out.println(view.getHand());
         backgroundImg = ImageIO.read(new File("src/main/resources/images/gamearea.png"));
-        BufferedImage img1= null;
-        if(view.getHand().get(0)<10){
-            img1 = ImageIO.read(new File("src/main/resources/images/small/front/00"+view.getHand().get(0) +".png"));
-        }
-        else {
-            img1 = ImageIO.read(new File("src/main/resources/images/small/front/0"+view.getHand().get(0) +".png"));
-        }
-        Icon ic1 = new ImageIcon(img1);
-        card1 = new JLabel(ic1);
 
-        //Drag and Drop delle carte
-        BufferedImage img2= null;
-        if(view.getHand().get(1)<10){
-            img2 = ImageIO.read(new File("src/main/resources/images/small/front/00"+view.getHand().get(1) +".png"));
-        }
-        else {
-            img2 = ImageIO.read(new File("src/main/resources/images/small/front/0"+view.getHand().get(1) +".png"));
-        }
-        Icon ic2 = new ImageIcon(img2);
-        card2 = new JLabel(ic1);
-
-
-        BufferedImage img3= null;
-        if(view.getHand().get(2)<10){
-            img3 = ImageIO.read(new File("src/main/resources/images/small/front/00"+view.getHand().get(2) +".png"));
-        }
-        else {
-            img3 = ImageIO.read(new File("src/main/resources/images/small/front/0"+view.getHand().get(2) +".png"));
-        }
-        Icon ic3 = new ImageIcon(img3);
-        card3 = new JLabel(ic3);
-
+        card1 = createCard(view.getHand().get(0));
+        card2 = createCard(view.getHand().get(1));
+        card3 = createCard(view.getHand().get(2));
 
         BufferedImage img4 = ImageIO.read(new File(objCard));
         Icon ic4 = new ImageIcon(img4);
         secretObjective = new JLabel(ic4);
+        cardStates.put(secretObjective, true); // Assuming secretObjective only has a front side
+        cardIds.put(secretObjective, -1); // No ID for secretObjective
 
-        switch (num){
+        switch (num) {
             case "1":
-                if(color.equals("Blue")){
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_bleu.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                } else if (color.equals("Yellow")) {
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_jaune.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                } else if (color.equals("Red")) {
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/red.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                } else {
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_vert.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                }
-                BufferedImage logo2 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_jaune.png"));
-                Icon icon2 = new ImageIcon(logo2);
-                token2 = new JLabel(icon2);
-                //token2.setVisible(false);
-                BufferedImage logo3 = ImageIO.read(new File("src/main/resources/images/red.png"));
-                Icon icon3 = new ImageIcon(logo3);
-                token3 = new JLabel(icon3);
-                token3.setVisible(false);
-                BufferedImage logo4 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_vert.png"));
-                Icon icon4 = new ImageIcon(logo4);
-                token4 = new JLabel(icon4);
-                token4.setVisible(false);
+                token1 = createToken(color, "Blue", "Yellow", "Red", "Green", true);
+                token2 = createToken("Yellow", "Blue", "Yellow", "Red", "Green", false);
+                token3 = createToken("Red", "Blue", "Yellow", "Red", "Green", false);
+                token4 = createToken("Green", "Blue", "Yellow", "Red", "Green", false);
                 break;
 
             case "2":
-                if(color.equals("Blue")){
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_bleu.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                } else if (color.equals("Yellow")) {
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_jaune.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                } else if (color.equals("Red")) {
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/red.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                } else {
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_vert.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                }
-                BufferedImage logo5 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_bleu.png"));
-                Icon icon5 = new ImageIcon(logo5);
-                token2 = new JLabel(icon5);
-                BufferedImage logo6 = ImageIO.read(new File("src/main/resources/images/red.png"));
-                Icon icon6 = new ImageIcon(logo6);
-                token3 = new JLabel(icon6);
-                token3.setVisible(false);
-                BufferedImage logo7 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_vert.png"));
-                Icon icon7 = new ImageIcon(logo7);
-                token4 = new JLabel(icon7);
-                token4.setVisible(false);
+                token1 = createToken(color, "Blue", "Yellow", "Red", "Green", true);
+                token2 = createToken("Blue", "Blue", "Yellow", "Red", "Green", true);
+                token3 = createToken("Red", "Blue", "Yellow", "Red", "Green", false);
+                token4 = createToken("Green", "Blue", "Yellow", "Red", "Green", false);
                 break;
 
             case "3":
-                if(color.equals("Blue")){
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_bleu.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                } else if (color.equals("Yellow")) {
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_jaune.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                } else if (color.equals("Red")) {
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/red.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                } else {
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_vert.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                }
-                BufferedImage logo8 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_jaune.png"));
-                Icon icon8 = new ImageIcon(logo8);
-                token2 = new JLabel(icon8);
-                BufferedImage logo9 = ImageIO.read(new File("src/main/resources/images/red.png"));
-                Icon icon9 = new ImageIcon(logo9);
-                token3 = new JLabel(icon9);
-                BufferedImage logo10 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_vert.png"));
-                Icon icon10 = new ImageIcon(logo10);
-                token4 = new JLabel(icon10);
-                token4.setVisible(false);
+                token1 = createToken(color, "Blue", "Yellow", "Red", "Green", true);
+                token2 = createToken("Yellow", "Blue", "Yellow", "Red", "Green", true);
+                token3 = createToken("Red", "Blue", "Yellow", "Red", "Green", true);
+                token4 = createToken("Green", "Blue", "Yellow", "Red", "Green", false);
                 break;
 
             case "4":
-                if(color.equals("Blue")){
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_bleu.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                } else if (color.equals("Yellow")) {
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_jaune.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                } else if (color.equals("Red")) {
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/red.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                } else {
-                    BufferedImage logo1 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_vert.png"));
-                    Icon icon1 = new ImageIcon(logo1);
-                    token1 = new JLabel(icon1);
-                }
-                BufferedImage logo11 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_jaune.png"));
-                Icon icon11 = new ImageIcon(logo11);
-                token2 = new JLabel(icon11);
-                BufferedImage logo12 = ImageIO.read(new File("src/main/resources/images/red.png"));
-                Icon icon12 = new ImageIcon(logo12);
-                token3 = new JLabel(icon12);
-                BufferedImage logo13 = ImageIO.read(new File("src/main/resources/images/CODEX_pion_vert.png"));
-                Icon icon13 = new ImageIcon(logo13);
-                token4 = new JLabel(icon13);
+                token1 = createToken(color, "Blue", "Yellow", "Red", "Green", true);
+                token2 = createToken("Yellow", "Blue", "Yellow", "Red", "Green", true);
+                token3 = createToken("Red", "Blue", "Yellow", "Red", "Green", true);
+                token4 = createToken("Green", "Blue", "Yellow", "Red", "Green", true);
                 break;
         }
-
-        //PlayCardArea
 
         playCardArea = new PlayCardArea() {
             ImageIcon icon = new ImageIcon(ImageIO.read(new File("src/main/resources/images/pannotavolo.jpg")));
             Image img = icon.getImage();
-            {setOpaque(false);}
-            public void paintComponent(Graphics graphics){
-                graphics.drawImage(img,0,0, this);
-                super.paintComponent(graphics);}
+            { setOpaque(false); }
+
+            public void paintComponent(Graphics graphics) {
+                graphics.drawImage(img, 0, 0, this);
+                super.paintComponent(graphics);
+            }
         };
 
         MouseAdapter ma = new MouseAdapter() {
-
             private Point origin;
 
             @Override
@@ -239,7 +126,6 @@ public class GameAreaPanel extends JPanel{
                     }
                 }
             }
-
         };
 
         playCardArea.addMouseListener(ma);
@@ -250,79 +136,162 @@ public class GameAreaPanel extends JPanel{
         jScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
         jScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
+        playCardArea.InsertCard(400, 400, starterCard);
 
-        playCardArea.InsertCard(400,400,starterCard);
-
-
+        addCardListeners(card1);
+        addCardListeners(card2);
+        addCardListeners(card3);
+        JLabel[] cards = {card1, card2, card3};
+        for (JLabel card : cards) {
+            card.setBorder(new LineBorder(Color.WHITE, 2));
+        }
         GridBagConstraints gbc = new GridBagConstraints();
 
         gbc.gridx = 1;
         gbc.gridy = 2;
         gbc.weightx = 0.0001;
         gbc.weighty = 0.000025;
-
         add(card1, gbc);
 
         gbc.gridx = 2;
         gbc.weightx = 0.0001;
-
         add(card2, gbc);
 
         gbc.gridx = 3;
         gbc.weightx = 0.0001;
-
         add(card3, gbc);
 
         gbc.gridx = 4;
         gbc.weightx = 0.0001;
-
         add(secretObjective, gbc);
 
         gbc.gridx = 0;
         gbc.gridy = 2;
-
         gbc.weightx = 0.1;
         gbc.weighty = 0.000025;
-
         add(token1, gbc);
 
         gbc.gridx = 5;
         gbc.gridy = 0;
-
         gbc.weightx = 0.1;
         gbc.weighty = 0.00025;
-
-
         add(token2, gbc);
 
         gbc.gridx = 0;
         gbc.gridy = 0;
-
         gbc.weightx = 0.000025;
         gbc.weighty = 0.000025;
-
         add(token3, gbc);
 
         gbc.gridx = 5;
         gbc.gridy = 2;
-
         gbc.weightx = 0.000025;
         gbc.weighty = 0.000025;
-
         add(token4, gbc);
 
         gbc.gridx = 1;
         gbc.gridy = 1;
-
         gbc.weightx = 0.7;
         gbc.weighty = 0.99;
-
         gbc.fill = GridBagConstraints.BOTH;
         gbc.gridwidth = 4;
-        //gbc.anchor = GridBagConstraints.LINE_START;
-
         add(jScrollPane, gbc);
 
+
+    }
+
+    private JLabel createCard(int cardId) throws IOException {
+        BufferedImage img;
+        if (cardId < 10) {
+            img = ImageIO.read(new File("src/main/resources/images/small/front/00" + cardId + ".png"));
+        } else {
+            img = ImageIO.read(new File("src/main/resources/images/small/front/0" + cardId + ".png"));
+        }
+        Icon icon = new ImageIcon(img);
+        JLabel cardLabel = new JLabel(icon);
+        cardStates.put(cardLabel, true); // Start with front side
+        cardIds.put(cardLabel, cardId);
+        return cardLabel;
+    }
+
+    private JLabel createToken(String color, String blueColor, String yellowColor, String redColor, String greenColor, boolean visible) throws IOException {
+        BufferedImage logo;
+        if (color.equals("Blue")) {
+            logo = ImageIO.read(new File("src/main/resources/images/CODEX_pion_bleu.png"));
+        } else if (color.equals("Yellow")) {
+            logo = ImageIO.read(new File("src/main/resources/images/CODEX_pion_jaune.png"));
+        } else if (color.equals("Red")) {
+            logo = ImageIO.read(new File("src/main/resources/images/red.png"));
+        } else {
+            logo = ImageIO.read(new File("src/main/resources/images/CODEX_pion_vert.png"));
+        }
+        Icon icon = new ImageIcon(logo);
+        JLabel tokenLabel = new JLabel(icon);
+        tokenLabel.setVisible(visible);
+        return tokenLabel;
+    }
+
+    private void addCardListeners(JLabel card) {
+        card.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 1) {
+                    selectCard(card);
+                } else if (e.getClickCount() == 2) {
+                    changeCardImage(card);
+                }
+            }
+        });
+    }
+
+
+
+    private void selectCard(JLabel card) {
+        if (selectedCard != null) {
+            selectedCard.setBorder(new LineBorder(Color.WHITE, 1));
+        }
+        selectedCard = card;
+        selectedCard.setBorder(new LineBorder(Color.BLUE, 2));
+        ChosenId = cardIds.get(card);
+        ChosenSide = cardStates.get(card) ? 0 : 1;
+    }
+
+    private void changeCardImage(JLabel card) {
+        try {
+            int cardId = cardIds.get(card);
+            boolean isFront = cardStates.get(card);
+            BufferedImage newImg = null;
+            if (isFront) {
+                if (cardId < 10) {
+                    newImg = ImageIO.read(new File("src/main/resources/images/small/back/001.png"));
+                } else if (cardId < 20) {
+                    newImg = ImageIO.read(new File("src/main/resources/images/small/back/011.png"));
+                } else if (cardId < 30) {
+                    newImg = ImageIO.read(new File("src/main/resources/images/small/back/021.png"));
+                } else if (cardId < 40) {
+                    newImg = ImageIO.read(new File("src/main/resources/images/small/back/031.png"));
+                } else if (cardId < 50) {
+                    newImg = ImageIO.read(new File("src/main/resources/images/small/back/041.png"));
+                } else if (cardId < 60) {
+                    newImg = ImageIO.read(new File("src/main/resources/images/small/back/051.png"));
+                } else if (cardId < 70) {
+                    newImg = ImageIO.read(new File("src/main/resources/images/small/back/061.png"));
+                } else if (cardId < 80) {
+                    newImg = ImageIO.read(new File("src/main/resources/images/small/back/071.png"));
+                }
+            } else {
+                if (cardId < 10) {
+                    newImg = ImageIO.read(new File("src/main/resources/images/small/front/00" + cardId + ".png"));
+                } else {
+                    newImg = ImageIO.read(new File("src/main/resources/images/small/front/0" + cardId + ".png"));
+                }
+            }
+            Icon newIcon = new ImageIcon(newImg);
+            card.setIcon(newIcon);
+            cardStates.put(card, !isFront); // Toggle state
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -337,6 +306,4 @@ public class GameAreaPanel extends JPanel{
             g.drawImage(backgroundImg, 0, 0, panelWidth, panelHeight, this);
         }
     }
-
-
 }
