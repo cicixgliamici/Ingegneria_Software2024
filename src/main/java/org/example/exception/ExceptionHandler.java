001580d355c91a7dbaeeb235ed0dbbb01464e7b4
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-06-04 16:27:48 +0200

    Update ExceptionHandler.java

--- src/main/java/org/example/exception/ExceptionHandler.java
+++ src/main/java/org/example/exception/ExceptionHandler.java
@@ -2,6 +2,16 @@ package org.example.exception;
 
 import org.example.server.Server;
 
+/**
+ * Interface for handling exceptions.
+ */
 public interface ExceptionHandler {
+    /**
+     * Handles the given exception.
+     * 
+     * @param exception The exception to be handled.
+     * @param username The username of the user who encountered the exception.
+     * @param server The server instance to interact with.
+     */
     void handle(Exception exception, String username, Server server);
 }
