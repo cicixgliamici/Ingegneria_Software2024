4c7801232eac414f180dbbd6a49d6afcd44feb1c
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-06-04 16:29:31 +0200

    Update InvalidCardExceptionHandler.java

--- src/main/java/org/example/exception/InvalidCardExceptionHandler.java
+++ src/main/java/org/example/exception/InvalidCardExceptionHandler.java
@@ -2,7 +2,17 @@ package org.example.exception;
 
 import org.example.server.Server;
 
+/**
+ * Handles InvalidCardException by notifying the server and user about the invalid card play.
+ */
 public class InvalidCardExceptionHandler implements ExceptionHandler {
+    /**
+     * Handles the given InvalidCardException.
+     * 
+     * @param exception The exception to be handled.
+     * @param username The username of the user who encountered the exception.
+     * @param server The server instance to interact with.
+     */
     public void handle(Exception exception, String username, Server server) {
         InvalidCardException ice = (InvalidCardException) exception;
         System.err.println("InvalidCardExceptionHandler: " + ice.getMessage());
