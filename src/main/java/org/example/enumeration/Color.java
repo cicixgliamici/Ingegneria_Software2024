cfc7140d5ba43ddf4ad3ba9a39204a065f6b2f81
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-06-26 20:59:11 +0200

    Commenti

--- src/main/java/org/example/enumeration/Color.java
+++ src/main/java/org/example/enumeration/Color.java
@@ -1,5 +1,9 @@
 package org.example.enumeration;
 
+/**
+ * Enum representing the possible colors of game pieces.
+ * The colors include White color for testing purposes
+ */
 public enum Color {
     BLUE, RED, YELLOW, GREEN, WHITE
 }
