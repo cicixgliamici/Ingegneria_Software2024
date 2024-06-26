25a840c6f236ccdbc1cff1b8f4950380db45688e
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-06-26 20:56:44 +0200

    Commenti

--- src/main/java/org/example/enumeration/CardRes.java
+++ src/main/java/org/example/enumeration/CardRes.java
@@ -1,5 +1,8 @@
 package org.example.enumeration;
 
+/**
+ * Enum representing the possible resources present on a corner of a card.
+ */
 public enum CardRes {
     PLANT, ANIMAL, FUNGI, INSECT, NULL, MANUSCRIPT, INKWELL, QUILL
 }
