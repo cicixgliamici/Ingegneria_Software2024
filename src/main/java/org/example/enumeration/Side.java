053fbae3a88ec44ce6643cb3153ae08c70a3f262
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-06-26 21:06:26 +0200

    Commenti

--- src/main/java/org/example/enumeration/Side.java
+++ src/main/java/org/example/enumeration/Side.java
@@ -1,5 +1,9 @@
 package org.example.enumeration;
 
+/**
+ * Enum representing the side of a card.
+ * BOTH - The card has not been played yet and both sides are considered.
+ */
 public enum Side {
     FRONT, BACK, BOTH
 }
