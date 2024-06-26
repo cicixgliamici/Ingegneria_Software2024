da937e4ef42593240d72233483f19cfe0fa82279
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-06-26 21:08:34 +0200

    Commenti

--- src/main/java/org/example/enumeration/StatusGame.java
+++ src/main/java/org/example/enumeration/StatusGame.java
@@ -1,5 +1,9 @@
 package org.example.enumeration;
 
+/**
+ * Enum representing the status of the game progression.
+ * NULL - The game is in the preliminary phase (connections and client setup).
+ */
 public enum StatusGame {
     NULL, DRAW, PLAY
 }
