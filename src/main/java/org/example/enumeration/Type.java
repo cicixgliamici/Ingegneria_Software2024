7f902483a16e2c9ce02a8c9cf5b4e13c1c984386
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-06-26 21:09:17 +0200

    Commenti

--- src/main/java/org/example/enumeration/Type.java
+++ src/main/java/org/example/enumeration/Type.java
@@ -1,5 +1,8 @@
 package org.example.enumeration;
 
+ /**
+ * Enum representing the types of decks and corresponding cards present in the game.
+ */
 public enum Type {
     RESOURCES, GOLD, OBJECT, STARTER;
 }
