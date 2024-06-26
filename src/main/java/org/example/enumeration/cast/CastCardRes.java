e5930ac1e2ffeed189e3cc86cfa69bfaeb7be6a0
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-06-26 20:54:06 +0200

    Commenti

--- src/main/java/org/example/enumeration/cast/CastCardRes.java
+++ src/main/java/org/example/enumeration/cast/CastCardRes.java
@@ -3,28 +3,39 @@ package org.example.enumeration.cast;
 import org.example.enumeration.CardRes;
 import org.example.enumeration.PropertiesCorner;
 
+/**
+ * The CastCardRes class maps a CardRes enumeration to its corresponding PropertiesCorner enumeration.
+ * This class takes a CardRes value as input and assigns the corresponding PropertiesCorner value based on predefined mappings.
+ */
 public class CastCardRes {
-    CardRes cardRes;
-    PropertiesCorner propertiesCorner;
+    private CardRes cardRes;
+    private PropertiesCorner propertiesCorner;
 
-    public CastCardRes (CardRes cardRes){
-        this.cardRes=cardRes;
-        if( this.cardRes == CardRes.PLANT){
-             this.propertiesCorner=PropertiesCorner.PLANT;
-        }
-        else if(this.cardRes == CardRes.ANIMAL){
-            this.propertiesCorner=PropertiesCorner.ANIMAL;
-        }
-        else if(this.cardRes == CardRes.FUNGI){
-             this.propertiesCorner=PropertiesCorner.FUNGI;
-        }
-        else {
-            this.propertiesCorner= PropertiesCorner.INSECT;
+    /**
+     * Constructs a CastCardRes instance with the specified CardRes value.
+     * Maps the CardRes value to its corresponding PropertiesCorner value.
+     * 
+     * @param cardRes the CardRes value to be mapped
+     */
+    public CastCardRes(CardRes cardRes) {
+        this.cardRes = cardRes;
+        if (this.cardRes == CardRes.PLANT) {
+            this.propertiesCorner = PropertiesCorner.PLANT;
+        } else if (this.cardRes == CardRes.ANIMAL) {
+            this.propertiesCorner = PropertiesCorner.ANIMAL;
+        } else if (this.cardRes == CardRes.FUNGI) {
+            this.propertiesCorner = PropertiesCorner.FUNGI;
+        } else {
+            this.propertiesCorner = PropertiesCorner.INSECT;
         }
     }
 
+    /**
+     * Returns the PropertiesCorner value corresponding to the CardRes value.
+     * 
+     * @return the PropertiesCorner value
+     */
     public PropertiesCorner getPropertiesCorner() {
         return propertiesCorner;
     }
 }
-
