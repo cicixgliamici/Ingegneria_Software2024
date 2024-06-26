139c44cfa9a7bb1aa9cedcf74e24bc9ff7cf1db1
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-06-26 21:05:35 +0200

    Commenti

--- src/main/java/org/example/enumeration/PropertiesCorner.java
+++ src/main/java/org/example/enumeration/PropertiesCorner.java
@@ -1,5 +1,8 @@
 package org.example.enumeration;
 
+/**
+ * Enum representing all possible items that can be present in a corner of a card.
+ */
 public enum PropertiesCorner {
     EMPTY, HIDDEN, PLANT, ANIMAL, FUNGI, INSECT, QUILL, INKWELL, MANUSCRIPT
 }
