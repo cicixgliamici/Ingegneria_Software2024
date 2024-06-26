b02a266ddade85ba045582cf015c32681cffb5d9
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-06-26 20:55:42 +0200

    Commenti

--- src/main/java/org/example/enumeration/CardPosition.java
+++ src/main/java/org/example/enumeration/CardPosition.java
@@ -1,5 +1,13 @@
 package org.example.enumeration;
 
+/**
+ * Enum representing the position of a card in various stages of the game.
+ * The card can be in one of the following positions:
+ * HAND - The card is in the player's hand.
+ * PLAYGROUND - The card is on the playground.
+ * DECKHIDDEN - The card is in the deck and hidden from view.
+ * DECKVISIBLE - The card is in the deck and visible to players.
+ */
 public enum CardPosition {
-    HAND, PLAYGORUND, DECKHIDDEN, DECKVISIBLE
+    HAND, PLAYGROUND, DECKHIDDEN, DECKVISIBLE
 }
