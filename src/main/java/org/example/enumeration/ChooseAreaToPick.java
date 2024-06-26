674019e017db539871b242c0228375c15858050c
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-06-26 20:57:56 +0200

    Commenti

--- src/main/java/org/example/enumeration/ChooseAreaToPick.java
+++ src/main/java/org/example/enumeration/ChooseAreaToPick.java
@@ -1,5 +1,11 @@
 package org.example.enumeration;
 
+/**
+ * Enum indicating the area from which the player draws a card.
+ * The player can draw a card from one of the following areas:
+ * >DECKSAREA - The player draws from the deck area.
+ * >VISIBLEAREA - The player draws from the visible cards area.
+ */
 public enum ChooseAreaToPick {
     DECKSAREA, VISIBLEAREA
 }
